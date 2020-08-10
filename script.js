17786357c133319acbd2f99129fe5ee66420ddac
shrushti2000 <shrushtipolekar@gmail.com>
2020-08-10 15:11:29 +0530

    created homepage ,admin login page ,ngo signup page and donate form

---
+++ script.js
@@ -0,0 +1,8 @@
+const tb=document.getElementsByClassName('toggle-button')[0];
+const navbarlinks =document.getElementsByClassName('navbar-links')[0];
+ 
+tb.addEventListener('click',()=>{
+    navbarlinks.classList.toggle('active')
+})
+
+

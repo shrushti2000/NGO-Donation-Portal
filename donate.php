08bb8e599535d12946336d39a8306ee5b8654e4f
Sshivani-12 <shivaniwakde1211@gmail.com>
2020-09-03 17:43:31 +0530

    making responsive

--- donate.php
+++ donate.php
@@ -2,7 +2,7 @@
     $host = "localhost:3307";
     $user = "root@";
     $pass = "";
-    $db = "ip";
+    $db = "main";
     $conn = mysqli_connect($host, $user, $pass, $db) or die('Error Connecting');
     
 
